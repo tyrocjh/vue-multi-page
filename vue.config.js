4b6e2a86a369cfca86da8c46639a4b1270f3fb84
ECNetBIOS\yulu <danielwithss@gmail.com>
2020-02-28 16:21:58 +0800

    auto open

--- vue.config.js
+++ vue.config.js
@@ -19,7 +19,7 @@ module.exports = {
       // 模板来源
       template: 'src/pages/template/index.html',
       // 在 dist/index.html 的输出
-      filename: 'pages/template.html',
+      filename: 'template.html',
       // 当使用 title 选项时，
       // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
       title: 'Template Page',
@@ -30,7 +30,7 @@ module.exports = {
     index: {
       entry: 'src/pages/index/index.js',
       template: 'src/pages/index/index.html',
-      filename: 'pages/index.html',
+      filename: 'index.html',
       title: 'Index Page',
       chunks: ['chunk-vendors', 'chunk-common', 'index']
     }
@@ -55,7 +55,7 @@ module.exports = {
 //         entries[tmp[1]] = {
 //             entry,
 //             template: htmls[tmp[1]] ? htmls[tmp[1]] : 'index.html', //  当前目录没有有html则以共用的public/index.html作为模板
-//             filename: 'pages/' + tmp[1] + '.html'   //  以文件夹名称.html作为访问地址
+//             filename: tmp[1] + '.html'   //  以文件夹名称.html作为访问地址
 //         };
 //     });
 //     console.log(entries)
